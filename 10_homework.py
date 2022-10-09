7b82bc5dd112ed271033c59b1be5db778219b28b
SSP0d <spodkopay@gmail.com>
2022-10-09 22:54:47 +0300

    Update 10_homework.py

--- 10_homework.py
+++ 10_homework.py
@@ -7,7 +7,7 @@ class AddressBook(UserDict):
 
 
 class Record:
-    def __init__(self, new_name) -> None:
+    def __init__(self, new_name):
         self.name = Name(new_name)
         self.phones = []
 
@@ -20,7 +20,7 @@ class Record:
                 self.phones.append(Phone(new_phone))
                 self.phones.remove(phone)
             else:
-                print("Phone number does't exist")
+                print("Phone number doesn't exist")  
 
     def remove_phone(self, old_phone):
         for phone in self.phones:
@@ -42,7 +42,7 @@ class Phone(Field):
     def __init__(self, phone) -> None:
         self.value = phone
 
-# DEFAULT_DICT = {}
+
 addressbook = AddressBook()
 
 
@@ -53,20 +53,12 @@ def input_error(func):
         except KeyError as error:
             print(f'Unknown command "{error.args[0]}". Try again..')
         except TypeError:
-            print(f'WrongD input. Try again..')
+            print(f'Wrong input. Try again..')
         except IndexError:
             print(f'invalid command syntax. Try again..')
         except ValueError as error:
             print(f'Error,{error.args[0]}. Try again...')
     return wrapper
-
-
-def hello_func(user_input):
-    print('How can I help you?')
-
-
-def show_all_func(user_input):
-    print(addressbook.data)
 
 
 @input_error
@@ -82,22 +74,60 @@ def get_handler(actions):
 
 @input_error
 def add_func(user_input):
-    addressbook.data[user_input[1]] = int(user_input[2])
-    print(f'New contact added')
+    if user_input[1] not in addressbook.data:
+        add_record = Record(user_input[1])
+        add_record.add_phone(user_input[2])
+        addressbook.add_record(add_record)
+        print(f'New contact added')
+    else:
+        add_phone = addressbook.data[1]
+        add_phone.add_phone(user_input[2])
+        print(f'New phone number to {user_input[1]} has been added')
 
 
 @input_error
 def change_func(user_input):
-    addressbook.data[user_input[1]] = int(user_input[2])
-    print(f'Phone number has been changed')
+    if user_input[1] in addressbook.data:
+        old_phone = input('Enter phone number to change: ')
+        renew_phone = addressbook.data[user_input[1]]
+        renew_phone.change_phone(old_phone, user_input[2])
+        print(f'Phone number has been changed')
+    else:
+        print(f"Phone number {user_input[2]} doesn't exist")
+
+
+@input_error
+def delete_func(user_input):
+    if user_input[1] in addressbook.data:
+        addressbook.data.pop(user_input[1])
+        print(f'Contact "{user_input[1]}" has been deleted')
+
+
+@input_error
+def remove_phone_func(user_input):
+    if user_input[1] in addressbook.data:
+        removing = addressbook.data[user_input[1]]
+        removing.remove_phone(user_input[2])
+        print(f'Phone number "{user_input[2]}" has been removed')
 
 
 @input_error
 def phone_func(user_input):
-    print(addressbook.data[user_input[1]])
+    if user_input[1] in addressbook.data:
+        print(f'{user_input[1]} has {addressbook.data[user_input[2]]} phone number')
+    else:
+        print(f"Contact '{user_input[1]}' doesn't exist")
 
 
-def break_func(user_input):
+def hello_func(*args):
+    print('How can I help you?')
+
+
+def show_all_func(*args):
+    print(addressbook.data)
+
+
+def break_func(*a):
     result = 'stop loop'
     print('Good bye!')
     return result
@@ -108,6 +138,8 @@ OPERATIONS = {
     'add': add_func,
     'change': change_func,
     'phone': phone_func,
+    'delete': delete_func,
+    'remove': remove_phone_func,
     'show all': show_all_func,
     'good bye': break_func,
     'close': break_func,
@@ -124,7 +156,7 @@ def main():
 
         if user_input == ['']:
             continue
-        elif user_input[0] in 'show':
+        elif user_input[0] == 'show':
             actions = 'show all'
         else:
             actions = user_input[0]
